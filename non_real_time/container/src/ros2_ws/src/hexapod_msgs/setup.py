0994faec70fb146b4bb4cb91bed069de7c11af88
zschwid <zschwid@gmail.com>
2026-06-06 18:01:15 -0400

    gait and IK pkgs begin

--- non_real_time/container/src/ros2_ws/src/hexapod_msgs/setup.py
+++ non_real_time/container/src/ros2_ws/src/hexapod_msgs/setup.py
@@ -1,29 +1,31 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'hexapod_msgs'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[],
     data_files=[
+        # Required so ROS2 can find the package
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        # 👇 THIS is the important part for message packages
+        (os.path.join('share', package_name, 'msg'), glob('msg/*.msg')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Hexapod shared message definitions',
+    license='TODO',
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
