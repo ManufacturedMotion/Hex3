b2cc8f08c1825fb6a1f405d72a3e7bfe946433de
Zack Schwid <zschwid@gmail.com>
2026-06-17 21:10:05 -0400

    backwards compatible rapid move

--- non_real_time/container/src/ros2_ws/src/tripod_gait/src/tripod_gait.cpp
+++ non_real_time/container/src/ros2_ws/src/tripod_gait/src/tripod_gait.cpp
@@ -151,7 +151,7 @@ void TripodGaitNode::updateGait(
                         adjusted_step_progress = step_progress < 0.5 ? 2.0 * step_progress : (step_progress - 0.5) * 2.0;
                         next_pos = (end_pos_ - start_pos_) * adjusted_step_progress + start_pos_;
                         next_pos.z -= -4 * adjusted_step_progress * (adjusted_step_progress - 1.0) * step_height_;
-                        rapidMove(next_pos, active_legs, true);
+                        rapidMove(next_pos, active_legs);
                     }
                 break;
                 case StepType::LINEAR_MOVE_RELATIVE:
