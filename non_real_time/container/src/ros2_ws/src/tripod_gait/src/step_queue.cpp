6cbe8115c70b332b15058b347ca14b7c640c3073
Zack Schwid <zschwid@gmail.com>
2026-06-19 13:40:10 -0400

    cmd_vel multiplier

--- non_real_time/container/src/ros2_ws/src/tripod_gait/src/step_queue.cpp
+++ non_real_time/container/src/ros2_ws/src/tripod_gait/src/step_queue.cpp
@@ -94,7 +94,7 @@ rclcpp::Duration StepQueue::enqueue(
         op_time);
 
     last_step_type_ = op_step_type;
-    RCLCPP_INFO_THROTTLE(rclcpp::get_logger(), *get_clock(), 1000, "Enqueued step of type %d", op_step_type);
+    // RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000, "Enqueued step of type %d", op_step_type);
 
     return op_time;
 }
