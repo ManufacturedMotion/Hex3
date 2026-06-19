a69e308e1c9c711b719544cbfa48ab2ced40efcf
Zack Schwid <zschwid@gmail.com>
2026-06-19 12:50:14 -0400

    cmd_vel receipt logging

--- non_real_time/container/src/ros2_ws/src/tripod_gait/src/gait.cpp
+++ non_real_time/container/src/ros2_ws/src/tripod_gait/src/gait.cpp
@@ -60,14 +60,14 @@ void Gait::cmdVelCallback(
     msg.angular.roll, 
     msg.angular.pitch, 
     msg.angular.yaw);
-    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000, 
-    "received cmd_vel x:%f; y:%f; z:%f, roll:%f, pitch:%f, yaw:%f", 
-    v_command.x, 
-    v_command.y, 
-    v_command.z, 
-    v_command.roll, 
-    v_command.pitch, 
-    v_command.yaw);
+    // RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000, 
+    // "received cmd_vel x:%f; y:%f; z:%f, roll:%f, pitch:%f, yaw:%f", 
+    // v_command.x, 
+    // v_command.y, 
+    // v_command.z, 
+    // v_command.roll, 
+    // v_command.pitch, 
+    // v_command.yaw);
 }
 
 void Gait::updateGait(
