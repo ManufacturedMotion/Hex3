4d05e96eea7943ce81e2da164070e9708b83ed38
Zack Schwid <zschwid@gmail.com>
2026-06-20 12:12:57 -0400

    sit macro

--- non_real_time/container/src/ros2_ws/src/control/control/xbox_joy_config.py
+++ non_real_time/container/src/ros2_ws/src/control/control/xbox_joy_config.py
@@ -57,65 +57,65 @@ class XboxJoyNode(Node):
                 self.macro_pub.publish(pub_msg)
         except IndexError:
             pass # self.last_joy has an empty buttons array to start
-        # left_y = -msg.axes[0]
-        # left_x = -msg.axes[1]
-        # right_x = -msg.axes[3]
-        # right_y = -msg.axes[2]
-        # dpad_horiz = msg.axes[6]
-        # dpad_vert = msg.axes[7]
+        left_y = -msg.axes[0]
+        left_x = -msg.axes[1]
+        right_x = -msg.axes[3]
+        right_y = -msg.axes[2]
+        dpad_horiz = msg.axes[6]
+        dpad_vert = msg.axes[7]
 
-        # threshold = 0.3
+        threshold = 0.3
 
-        # # --- RIGHT STICK: 8-way snap, only 0, ±0.5, ±1 ---
-        # mag_right = math.hypot(right_y, right_x)
-        # discrete_x, discrete_y = 0.0, 0.0
-        # if mag_right > threshold:
-        #     angle = math.atan2(right_y, right_x)
-        #     direction = int(((math.degrees(angle) + 360 + 22.5) % 360) // 45)
-        #     match direction:
-        #         case 0:
-        #             discrete_x, discrete_y = 1.0, 0.0
-        #         case 1:
-        #             discrete_x, discrete_y = 0.5, 0.5
-        #         case 2:
-        #             discrete_x, discrete_y = 0.0, 1.0
-        #         case 3:
-        #             discrete_x, discrete_y = -0.5, 0.5
-        #         case 4:
-        #             discrete_x, discrete_y = -1.0, 0.0
-        #         case 5:
-        #             discrete_x, discrete_y = -0.5, -0.5
-        #         case 6:
-        #             discrete_x, discrete_y = 0.0, -1.0
-        #         case 7:
-        #             discrete_x, discrete_y = 0.5, -0.5
+        # --- RIGHT STICK: 8-way snap, only 0, ±0.5, ±1 ---
+        mag_right = math.hypot(right_y, right_x)
+        discrete_x, discrete_y = 0.0, 0.0
+        if mag_right > threshold:
+            angle = math.atan2(right_y, right_x)
+            direction = int(((math.degrees(angle) + 360 + 22.5) % 360) // 45)
+            match direction:
+                case 0:
+                    discrete_x, discrete_y = 1.0, 0.0
+                case 1:
+                    discrete_x, discrete_y = 0.5, 0.5
+                case 2:
+                    discrete_x, discrete_y = 0.0, 1.0
+                case 3:
+                    discrete_x, discrete_y = -0.5, 0.5
+                case 4:
+                    discrete_x, discrete_y = -1.0, 0.0
+                case 5:
+                    discrete_x, discrete_y = -0.5, -0.5
+                case 6:
+                    discrete_x, discrete_y = 0.0, -1.0
+                case 7:
+                    discrete_x, discrete_y = 0.5, -0.5
 
-        #     discrete_y *= mag_right
-        #     discrete_x *= mag_right
+            discrete_y *= mag_right
+            discrete_x *= mag_right
 
-        # # --- LEFT STICK: Only one dominant direction, never both ---
-        # mag_left = math.hypot(left_y, left_x)
-        # lin_x, ang_z = 0.0, 0.0
-        # if abs(left_x) > threshold or abs(left_y) > threshold:
-        #     if abs(left_x) > abs(left_y):
-        #         lin_x = math.copysign(1.0, left_x)
-        #         ang_z = 0.0
-        #     else:
-        #         ang_z = math.copysign(1.0, left_y)
-        #         lin_x = 0.0
+        # --- LEFT STICK: Only one dominant direction, never both ---
+        mag_left = math.hypot(left_y, left_x)
+        lin_x, ang_z = 0.0, 0.0
+        if abs(left_x) > threshold or abs(left_y) > threshold:
+            if abs(left_x) > abs(left_y):
+                lin_x = math.copysign(1.0, left_x)
+                ang_z = 0.0
+            else:
+                ang_z = math.copysign(1.0, left_y)
+                lin_x = 0.0
         
-        #     ang_z *= mag_left
-        #     lin_x *= mag_left
+            ang_z *= mag_left
+            lin_x *= mag_left
 
-        # twist = Twist()
-        # twist.linear.y = discrete_y
-        # twist.linear.x = discrete_x if mag_right > threshold else lin_x
-        # twist.angular.z = ang_z
-        # twist.angular.y = dpad_horiz
-        # twist.angular.x = dpad_vert
+        twist = Twist()
+        twist.linear.y = discrete_y
+        twist.linear.x = discrete_x if mag_right > threshold else lin_x
+        twist.angular.z = ang_z
+        twist.angular.y = dpad_horiz
+        twist.angular.x = dpad_vert
 
-        # self.last_twist = twist
-        # self.last_joy_time = self.get_clock().now()
+        self.last_twist = twist
+        self.last_joy_time = self.get_clock().now()
         self.last_joy = msg
 
     def poll_callback(self):
