a3c970710c1ce7f512342675672203facd61c4a3
Zack Schwid <zschwid@gmail.com>
2026-06-28 12:57:22 -0400

    direction vector handling

--- non_real_time/container/src/ros2_ws/src/tripod_gait/src/tripod_gait.cpp
+++ non_real_time/container/src/ros2_ws/src/tripod_gait/src/tripod_gait.cpp
@@ -342,9 +342,9 @@ double TripodGaitNode::getMaxStepMagnitudeInDirection_(Pose6D direction_vector, 
 	buffer1.z = 0.00; // For now we don't consider Z, roll, or pitch
 	buffer1.roll = 0.00;
 	buffer1.pitch = 0.00;
-	if (flipped_step_group) {
-		buffer1 *= -1.0; // If the step group has been flipped, then the previous step was in the opposite direction
-	}
+	// if (flipped_step_group) {
+	// 	buffer1 *= -1.0; // If the step group has been flipped, then the previous step was in the opposite direction
+	// }
 
     direction_vector.z = 0.0;  // For now we don't consider Z, roll, or pitch
     direction_vector.roll = 0.0;
