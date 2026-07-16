9d97ad2b6dd246d38983fd374f81fe97ad7b7794
Daniel Iacobacci <66583553+Iacobacci@users.noreply.github.com>
2026-07-16 16:04:17 -0700

    do not always build

--- non_real_time/container/src/entry_point.sh
+++ non_real_time/container/src/entry_point.sh
@@ -6,9 +6,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 
 cd /app/src/ros2_ws
 
-# Build the full workspace so the latest nodes are installed before launch.
-colcon build
-
 # Source the local workspace
 source install/local_setup.bash
 
