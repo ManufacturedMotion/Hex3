0c16a797882f4976f17476b65a01041a6ac3da8e
Zack Schwid <zschwid@gmail.com>
2026-07-17 00:13:31 -0700

    wave

--- non_real_time/container/src/ros2_ws/src/hexapod_manager/launch/hexapod.launch.py
+++ non_real_time/container/src/ros2_ws/src/hexapod_manager/launch/hexapod.launch.py
@@ -3,6 +3,8 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+os.environ['ROS_DOMAIN_ID'] = '12'
+
 def generate_launch_description():
 
     ik_config_file = os.path.join(
